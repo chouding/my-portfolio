853b9a00ab15f2833a77477b8034096ee9f72a67
chouding <raymond.chou523@gmail.com>
2025-06-08 17:43:16 -0700

    Added experience section and data

--- lib/data.ts
+++ lib/data.ts
@@ -35,28 +35,46 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Front-end Engineer AWS Intern",
+    company: "Amazon AWS",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "Incoming Front-End Engineer AWS Intern.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "June 2025 - Present",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Software Engineer Intern",
+    company: "IMD Solutions",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      `• Developed a scalable React Native mobile application targeting 1.6+ million Type 1 diabetics, integrating Dexcom API
+       • Handled complex data processing and storage for 100+ beta users by building RESTful APIs using Flask
+       • Ensured reliable message delivery by implementing a secure OAuth flow and token management using Firebase
+       • Reduced critical glucose alert response times by 89% by implementing multi-threading to optimize alert pipeline
+       • Implemented a deep learning model for blood glucose forecasting, reducing hypoglycemic events by 25%`,
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "Jan 2025 - June 2025",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Software Engineer Intern",
+    company: "Amgen Inc.",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      `• Led a research project to evaluate the performance of a RAG-based chatbot on the Amgen Sensing platform
+       • Built a Databricks-based evaluation pipeline using Ragas and OpenAI, improving response relevance
+       • Analyzed 100+ chatbot interactions, enhancing response consistency across a multitude of different departments
+       • Aligned evaluation metrics with business goals, driving 50%+ adoption of the metric by developers`,
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "June 2024 - Sept 2024",
+  },
+    {
+    title: "Web Developer",
+    company: "UCI Division of Equal Opportunity and Compliance",
+    description:
+      `• Reduced migration time of policy website by 30% through efficient usage of Cascade CMS, a web layout builder
+       • Improved the website’s interactivity and design, accelerating the work efficiency of 10+ employees
+       • Developed scalable and reusable UI components through Velocity Templates, increasing website traffic by 15%
+       • Successfully met WCAG 2.0/2.1 AA standards to make web content more accessible and visible for users`,
+    icon: React.createElement(FaReact),
+    date: "June 2023 - June 2024",
   },
 ] as const;
 
