8ea24149fbfef3914aa6cb50fe7bf4a69cd1f283
chouding <raymond.chou523@gmail.com>
2025-06-08 17:16:12 -0700

    About section

---
+++ components/About.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./ui/SectionHeading";
+import { motion } from "framer-motion";
+
+export default function About() {
+  return (
+    <motion.section 
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0}}
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        I'm currently a senior at UC Irvine studying Computer Science and Software Engineering. I am excited to announce that I will be joining the DC Console team this summer as a Front-End Engineer AWS Intern!
+        In the past, I've had the opportunity to contribute to projects aimed at solving real-world problems using my full-stack skills. Most recently, I completed my internship focused on preventing hypoglycemic episodes in individuals with Type 1 diabetes.
+      </p>
+      <p>
+        Outside of coding, I’m a huge NBA fan (go Warriors!) and love playing basketball, enjoying outdoor activities like hiking/jogging (if the weather's nice, of course), and exploring new places. This is actually my first time in Colorado, so I can't wait to see what there is to do here!
+      </p>
+    </motion.section>
+  )
+}
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -46,7 +46,7 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-      <motion.p 
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4l"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -61,7 +61,7 @@ export default function Intro() {
         <span>
           with experience solving real-world problems.
         </span>
-      </motion.p>
+      </motion.h1>
       <motion.div 
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100  }}
